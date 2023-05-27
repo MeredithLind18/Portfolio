b59ba1ed37ee79bf11450bb7c5c20947275a96c1
MeredithLind18 <117303545+MeredithLind18@users.noreply.github.com>
2023-05-27 15:29:07 -0600

    js gif rotation changes

--- animations.js
+++ animations.js
@@ -1,1 +1,6 @@
-
+$(document).ready(function() {
+  $('.hero-container_img').addClass('rotate-animation');
+  setTimeout(function() {
+    $('.hero-container_img').removeClass('rotate-animation');
+  }, 2000);
+});
