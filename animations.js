3dc2e3e9b1f5a72a444b5e18a2c2a3c877ce413b
MeredithLind18 <117303545+MeredithLind18@users.noreply.github.com>
2023-08-10 10:00:08 -0600

    Delete animations.js

--- animations.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $('.hero-container_img').addClass('rotate-animation');
-  setTimeout(function() {
-    $('.hero-container_img').removeClass('rotate-animation');
-  }, 2000);
-});
